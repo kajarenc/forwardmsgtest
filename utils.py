63db83ff799427475cc38195e043119ef8a7db2c
Karen Javadyan <kajarenc@gmail.com>
2023-11-28 22:11:18 +0400

    increase timeout, better exception text

--- utils.py
+++ utils.py
@@ -2,11 +2,11 @@ import requests
 
 def download_file(url):
     try:
-        response =  requests.get(url, timeout=5)
+        response =  requests.get(url, timeout=10)
     except Exception as e:
         raise e
     if response.status_code == 200:
         return response.text
     else:
-        raise Exception("Invalid Streamlit app URL")
+        raise Exception(f"Bad status code: {response.status_code}")
     
